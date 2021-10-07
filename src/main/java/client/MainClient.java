731155ef0c1bf76309b7d7caaeca2b95391ca904
Shamayko <shamayko@gmail.com>
2021-10-07 16:25:38 +0300

    First commit

---
+++ src/main/java/client/MainClient.java
@@ -0,0 +1,25 @@
+package client;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainClient extends Application {
+    public MainClient() {
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("/main.fxml"));
+        Parent root = (Parent)fxmlLoader.load();
+        primaryStage.setTitle("CloudiA - Dropbox primitive imitation");
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
